45ba68cafafa4929720ee52318bdc5e050e9b096
Kim Ly Buck <kim.3993@gmail.com>
2020-03-22 20:20:11 -0500

    styled search box

--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -1,10 +1,18 @@
 import React from 'react';
+import styled from 'styled-components';
+
+const Search = styled.div`
+	text-align: center;
+`;
+const SearchBox = styled.input`
+	margin-left: 2vw;	
+`;
 
 export default function SearchForm(props) {
 	return (
-		<section className="search-form">
+		<Search className="search-form">
 			<label htmlFor="search">Search by Name</label>
-			<input
+			<SearchBox
 				type="text"
 				name="search"
 				value={props.query}
@@ -12,6 +20,6 @@ export default function SearchForm(props) {
 					props.setQuery(event.target.value);
 				}}
 			/>
-		</section>
+		</Search>
 	);
 }

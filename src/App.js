1edee8a1f7ba9a584e413af5edf9b5293a0889b2
Kim Ly Buck <kim.3993@gmail.com>
2020-03-22 19:14:09 -0500

    Added router

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
-import React from "react";
-import Header from "./components/Header.js";
+import React from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import Header from './components/Header.js';
+import CharacterList from './components/CharacterList.js';
+import WelcomePage from './components/WelcomePage.js';
 
 export default function App() {
-  return (
-    <main data-testid='app'>
-      <Header />
-    </main>
-  );
+	return (
+		<main data-testid="app">
+			<Router>
+				<Header />
+				<Route exact path="/" component={WelcomePage} />
+				<Route path="/characters" component={CharacterList} />
+			</Router>
+		</main>
+	);
 }
